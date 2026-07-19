fcfa82654e28876fdc406331b86d7bc5d76aca2c
avv77 <aleks.vv00@gmail.com>
2026-07-19 18:33:44 +0300

    codecrafters submit [skip ci]

--- app/storage/info.go
+++ app/storage/info.go
@@ -1,5 +1,7 @@
 package storage
 
+import "github.com/codecrafters-io/redis-starter-go/app/utils"
+
 type ServerInfo struct {
 	data map[string]string
 }
@@ -10,17 +12,28 @@ func NewServerInfoStore() *ServerInfo {
 	}
 }
 
+const roleKey string = "role"
+const masterReplicaIdKey string = "master_replid"
+const masterReplicaOffsetKey string = "master_repl_offset"
+
 func (s *ServerInfo) SetReplicationInfo(isMaster bool) {
 	role := "slave"
 	if isMaster {
 		role = "master"
 	}
 
-	s.data["role"] = role
+	s.data[roleKey] = role
+
+	if isMaster {
+		s.data[masterReplicaIdKey] = utils.RandString(40)
+		s.data[masterReplicaOffsetKey] = "0"
+	}
 }
 
 var replicationInfoKeys map[string]any = map[string]any{
-	"role": struct{}{},
+	roleKey:                struct{}{},
+	masterReplicaIdKey:     struct{}{},
+	masterReplicaOffsetKey: struct{}{},
 }
 
 func (s *ServerInfo) GetReplicationInfo() map[string]string {
--- app/utils/utils.go
+++ app/utils/utils.go
@@ -1,5 +1,17 @@
 package utils
 
+import "math/rand/v2"
+
 func ToPtr[T any](v T) *T {
 	return &v
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func RandString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.IntN(len(charset))]
+	}
+	return string(b)
 }
